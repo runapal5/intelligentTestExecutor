c91bbbb151fa844a37984312e214284567a351dd
runapal5 <runapal5@gmail.com>
2018-02-07 07:48:37 +0000

    Add files via upload

---
+++ document_clustering-kMeans-sita.py
@@ -0,0 +1,244 @@
+import pandas as pd
+import numpy as np
+
+import csv
+
+#------------------------Parsing Document Files and storing data in dictionary ------------------------------#
+
+from docx import Document
+
+
+import os
+
+usecasespathDir = "./usecases/"
+
+document_list = []
+
+document_list = [x for x in os.listdir(usecasespathDir) if x.endswith(".docx")]
+print document_list
+
+documentText_list = []
+
+for documentName in document_list:
+    document = Document(usecasespathDir+documentName)
+    documentText = ''
+    try:
+        for para in document.paragraphs:
+            # print para.text
+            documentText = documentText + para.text
+    except:
+           print "Exception in document ", documentName
+    documentText_list.append(documentText)
+
+
+
+
+documents_dict = {}
+documents_dict['Title'] = document_list
+documents_dict['Synopsis'] = documentText_list
+
+
+movie_data = pd.DataFrame.from_dict(documents_dict)
+#print movie_data
+
+#print movie_data['Title'].tolist()
+
+
+
+movie_titles = movie_data['Title'].tolist()
+movie_synopses = movie_data['Synopsis'].tolist()
+
+
+#print 'Movie:', movie_titles[0]
+#print 'Movie Synopsis:', movie_synopses[0][:58]
+
+
+#------------------------Parsing Document Files and storing data in dictionary ------------------------------#
+
+#-------------------------Parsing From CSV File -----------------------------------------#
+"""
+movie_data = pd.read_csv('data.csv')
+#print movie_data
+movie_data = pd.read_csv('sitausecases.csv')
+
+print movie_data.head()
+print movie_data['Synopsis']
+
+
+
+movie_titles = movie_data['Title'].tolist()
+movie_synopses = movie_data['Synopsis'].tolist()
+
+
+print 'Movie:', movie_titles[0]
+print 'Movie Synopsis:', movie_synopses[0][:58]
+"""
+#-------------------------Parsing From CSV File -----------------------------------------#
+
+
+from normalization import normalize_corpus
+from utils import build_feature_matrix
+
+# normalize corpus
+norm_movie_synopses = normalize_corpus(movie_synopses,
+                                       lemmatize=True,
+                                       only_text_chars=True)
+
+# extract tf-idf features
+vectorizer, feature_matrix = build_feature_matrix(norm_movie_synopses,
+                                                  feature_type='tfidf',
+                                                  min_df=0.24, max_df=0.85,
+                                                  ngram_range=(1, 2))
+# view number of features
+print "Feature Matrix::::" ,feature_matrix.shape     
+
+# get feature names
+feature_names = vectorizer.get_feature_names()
+
+# print sample features
+print "Feature Names:::::" , feature_names[:20]      
+
+                    
+from sklearn.cluster import KMeans
+
+def k_means(feature_matrix, num_clusters=5):
+    #km = KMeans(n_clusters=num_clusters, max_iter=10000)
+    km = KMeans(n_clusters=num_clusters)
+    km.fit(feature_matrix)
+    clusters = km.labels_
+    return km, clusters
+
+num_clusters = 5    
+km_obj, clusters = k_means(feature_matrix=feature_matrix,
+                           num_clusters=num_clusters)
+
+print clusters
+
+movie_data['Cluster'] = clusters
+
+
+from collections import Counter
+# get the total number of movies per cluster
+c = Counter(clusters)
+print c.items()
+
+
+def get_cluster_data(clustering_obj, movie_data, 
+                     feature_names, num_clusters,
+                     topn_features=10):
+
+    cluster_details = {}  
+    # get cluster centroids
+    ordered_centroids = clustering_obj.cluster_centers_.argsort()[:, ::-1]
+    # get key features for each cluster
+    # get movies belonging to each cluster
+    for cluster_num in range(num_clusters):
+        cluster_details[cluster_num] = {}
+        cluster_details[cluster_num]['cluster_num'] = cluster_num
+        key_features = [feature_names[index] 
+                        for index 
+                        in ordered_centroids[cluster_num, :topn_features]]
+        cluster_details[cluster_num]['key_features'] = key_features
+        #print "cluster_number:",cluster_num
+        #print "helll:::",movie_data['Cluster']
+        movies = movie_data[movie_data['Cluster'] == cluster_num]['Title'].values.tolist()
+        cluster_details[cluster_num]['movies'] = movies
+    
+    return cluster_details
+        
+       
+    
+def print_cluster_data(cluster_data):
+    # print cluster details
+    for cluster_num, cluster_details in cluster_data.items():
+        print 'Cluster {} details:'.format(cluster_num)
+        print '-'*20
+        print 'Key features:', cluster_details['key_features']
+        print 'Movies in this cluster:'
+        print ', '.join(cluster_details['movies'])
+        print '='*40
+
+
+import matplotlib.pyplot as plt
+from sklearn.manifold import MDS
+from sklearn.metrics.pairwise import cosine_similarity
+import random
+from matplotlib.font_manager import FontProperties
+
+def plot_clusters(num_clusters, feature_matrix,
+                  cluster_data, movie_data,
+                  plot_size=(16,8)):
+    # generate random color for clusters                  
+    def generate_random_color():
+        color = '#%06x' % random.randint(0, 0xFFFFFF)
+        return color
+    # define markers for clusters    
+    markers = ['o', 'v', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'd']
+    # build cosine distance matrix
+    cosine_distance = 1 - cosine_similarity(feature_matrix) 
+    # dimensionality reduction using MDS
+    mds = MDS(n_components=2, dissimilarity="precomputed", 
+              random_state=1)
+    # get coordinates of clusters in new low-dimensional space
+    plot_positions = mds.fit_transform(cosine_distance)  
+    x_pos, y_pos = plot_positions[:, 0], plot_positions[:, 1]
+    # build cluster plotting data
+    cluster_color_map = {}
+    cluster_name_map = {}
+    for cluster_num, cluster_details in cluster_data.items():
+        # assign cluster features to unique label
+        cluster_color_map[cluster_num] = generate_random_color()
+        cluster_name_map[cluster_num] = ', '.join(cluster_details['key_features'][:5]).strip()
+    # map each unique cluster label with its coordinates and movies
+    cluster_plot_frame = pd.DataFrame({'x': x_pos,
+                                       'y': y_pos,
+                                       'label': movie_data['Cluster'].values.tolist(),
+                                       'title': movie_data['Title'].values.tolist()
+                                        })
+    grouped_plot_frame = cluster_plot_frame.groupby('label')
+    # set plot figure size and axes
+    fig, ax = plt.subplots(figsize=plot_size) 
+    ax.margins(0.05)
+    # plot each cluster using co-ordinates and movie titles
+    for cluster_num, cluster_frame in grouped_plot_frame:
+         marker = markers[cluster_num] if cluster_num < len(markers) \
+                  else np.random.choice(markers, size=1)[0]
+         ax.plot(cluster_frame['x'], cluster_frame['y'], 
+                 marker=marker, linestyle='', ms=12,
+                 label=cluster_name_map[cluster_num], 
+                 color=cluster_color_map[cluster_num], mec='none')
+         ax.set_aspect('auto')
+         ax.tick_params(axis= 'x', which='both', bottom='off', top='off',        
+                        labelbottom='off')
+         ax.tick_params(axis= 'y', which='both', left='off', top='off',         
+                        labelleft='off')
+    fontP = FontProperties()
+    fontP.set_size('small')    
+    ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.01), fancybox=True, 
+              shadow=True, ncol=5, numpoints=1, prop=fontP) 
+    #add labels as the film titles
+    for index in range(len(cluster_plot_frame)):
+        ax.text(cluster_plot_frame.ix[index]['x'], 
+                cluster_plot_frame.ix[index]['y'], 
+                cluster_plot_frame.ix[index]['title'], size=8)  
+    plt.savefig('clusters_data.png', dpi=200)
+    # show the plot           
+    plt.show() 
+
+
+cluster_data =  get_cluster_data(clustering_obj=km_obj,
+                                 movie_data=movie_data,
+                                 feature_names=feature_names,
+                                 num_clusters=num_clusters,
+                                 topn_features=5)         
+
+print_cluster_data(cluster_data) 
+
+plot_clusters(num_clusters=num_clusters, 
+              feature_matrix=feature_matrix,
+              cluster_data=cluster_data, 
+              movie_data=movie_data,
+              plot_size=(16,8))       
+              
+
+
